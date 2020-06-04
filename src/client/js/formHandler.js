9abc1f19d3e0ce855d7c379f0e2c715641e940ef
Enid-Sky <enid.sky@protonmail.com>
2020-06-04 11:09:01 -0700

    add API

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,12 @@
+// API Key for OpenWeatherMap API
+
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+
+const country = ',US&units=imperial'
+
+const apiKey = '&APPID=e131d5ea093dbc7fc2da1a0496c042c8';
+
+
 function handleSubmit(event) {
     event.preventDefault()
 
@@ -6,12 +15,29 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-        .then(res => res.json())
-        .then(function (res) {
-            document.getElementById('results').innerHTML = res.message
-        })
+
+    /*Get API Data*/
+    const getDate = async (baseURL, zip, country, apiKey)
+    fetch(baseURL + zip + country + apiKey);
+    try {
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        console.log('error '
+            err);
+    }
 }
+
+getDate(baseURL, zip, country, apiKey)
+    .then(function (data) {
+        postData('/add', {
+            date: newDate,
+        })
+    })
+    .then(function (res) {
+        document.getElementById('results').innerHTML = res.date;
+    })
+
 
 export {
     handleSubmit
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,26 @@
+projectData = {};
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
-const app = express()
+const app = express();
 
+//initialize the main folder
 app.use(express.static('dist'))
+
+/*Middleware*/
+//Configure express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(body - bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(body - parser.json());
+
+//cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 console.log(__dirname)
 
@@ -17,6 +33,21 @@ app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
+//get route
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+    res.send(projectData)
 })
+
+//POST route
+
+app.post('/add', addInfo);
+
+function addInfo(req, res) {
+    let data = req.body;
+    newEntry = {
+        temp: data.main.temp
+    }
+
+    Object.assign(projectData, newEntry);
+    res.send(projectData)
+}
