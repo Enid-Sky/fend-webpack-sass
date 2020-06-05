64e037175cf9b547e7fe4ea332b329fb02a57b0e
Enid-Sky <enid.sky@protonmail.com>
2020-06-05 10:00:06 -0700

    add console message for updating UI

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,15 +3,13 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-        .then(res => {
-            return res.json()
-        })
+    fetch('http://localhost:8080/test')
+        .then(res => res.json())
         .then(function (data) {
+            console.log('updating the UI')
             document.getElementById('results').innerHTML = data.message
         })
 }
