d917bceec245bdb14d53c6fe202957e267c31cc5
Enid-Sky <enid.sky@protonmail.com>
2020-06-09 14:11:41 -0700

    add console.log form submitted

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,7 +3,10 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
+    console.log('::: Returned user input ::: ' + formText)
     Client.checkForName(formText)
+    console.log("::: Form Submitted :::");
+
 
     console.log("::: Form Submitted :::")
     fetch('http://localhost:8080/test')
