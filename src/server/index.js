6cd343e86d331abb611f2b52a2e30d53dcada51f
Enid-Sky <enid.sky@protonmail.com>
2020-06-09 15:42:29 -0700

    delete unused modules

--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,14 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-var bodyParser = require('body-parser')
-var cors = require('cors')
 
-/*var json = {
-    'title': 'test json response',
-    'message': 'this is a message',
-    'time': 'now'
-}
-*/
+
 const app = express()
-app.use(cors())
-// to use json
-app.use(bodyParser.json())
-// to use url encoded values
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
+
 
 app.use(express.static('dist'))
+
+console.log(__dirname)
 
 //console.log(JSON.stringify(mockAPIResponse))
 
@@ -27,11 +16,11 @@ app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 })
 
-app.get('/test', function (req, res) {
-    res.json(mockAPIResponse);
+// designates what port the app will listen to for incoming requests
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
 })
 
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse);
 })
