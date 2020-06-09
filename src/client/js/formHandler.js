bfe429b79b6eede1c8473f65c0421adc00e5eecf
Enid-Sky <enid.sky@protonmail.com>
2020-06-09 15:43:08 -0700

    update local host to 3000

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,17 +3,13 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    console.log('::: Returned user input ::: ' + formText)
     Client.checkForName(formText)
-    console.log("::: Form Submitted :::");
-
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    fetch('http://localhost:3000/test')
         .then(res => res.json())
-        .then(function (data) {
-            console.log('updating the UI')
-            document.getElementById('results').innerHTML = data.message
+        .then(function (res) {
+            document.getElementById('results').innerHTML = res.message
         })
 }
 
